2caf3c4ea279fa2436cdcd89a23c9a2ce0033ff3
alkuramshina <alkuramshina@gmail.com>
2023-10-30 22:12:41 +0300

    ! bugfixes

---
+++ Assets/Scripts/Spawn/UnitSpawner.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using Configuration;
+using UnityEngine;
+
+namespace Spawn
+{
+    public class UnitSpawner : ClickableElement
+    {
+        [SerializeField, Tooltip("Стартовая цель")]
+        private Transform defaultTarget;
+    
+        [SerializeField, Tooltip("Базовая конфигурация юнитов")]
+        private UnitSettings baseUnitSettings;
+    
+        [SerializeField, Tooltip("Материал юнитов")] 
+        private Material baseMaterial;
+    
+        [SerializeField, Tooltip("Место спауна")] 
+        private Transform unitSpawnPoint;
+    
+        [SerializeField, Tooltip("Время между спаунами, сек.")] 
+        private float spawnWaitingTime;
+    
+        [SerializeField, Tooltip("Максимум юнитов")] 
+        private int maxUnitLimit;
+
+        private readonly List<Unit> _spawnedUnits = new();
+        private float _spawnTimer;
+
+        protected override void Update()
+        {
+            base.Update();
+        
+            TrySpawn();
+        }
+
+        private void TrySpawn()
+        {
+            if (_spawnedUnits.Count >= maxUnitLimit)
+            {
+                return;
+            }
+        
+            _spawnTimer += Time.deltaTime;
+            if (_spawnTimer < spawnWaitingTime)
+            {
+                return;
+            }
+        
+            var newUnit = Unit.Spawn(baseUnitSettings, baseMaterial, unitSpawnPoint, defaultTarget);
+            _spawnedUnits.Add(newUnit);
+        
+            _spawnTimer = 0f;
+        }
+
+        public void ChangeUnitSettings(UnitSettings newSettings)
+        {
+            baseUnitSettings.speed = newSettings.speed;
+            baseUnitSettings.maxHealth = newSettings.maxHealth;
+            baseUnitSettings.critChance = newSettings.critChance;
+            baseUnitSettings.missChance = newSettings.missChance;
+            baseUnitSettings.quickAttack = newSettings.quickAttack;
+            baseUnitSettings.strongAttack = newSettings.strongAttack;
+        }
+
+        public UnitSettings GetCurrentSettings()
+            => baseUnitSettings;
+    }
+}
---
+++ Assets/Scripts/Spawn/UnitSpawnerSettingsUI.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections;
+using Configuration;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Utils;
+
+namespace Spawn
+{
+    public class UnitSpawnerSettingsUI : MonoBehaviour
+    {
+        [SerializeField] private UnitSpawner spawner;
+    
+        [SerializeField] private TMP_InputField maxHealthInput;
+        [SerializeField] private TMP_InputField speedInput;
+        [SerializeField] private TMP_InputField missChanceInput;
+        [SerializeField] private TMP_InputField critChanceInput;
+        [SerializeField] private TMP_InputField quickAttackDamageInput;
+        [SerializeField] private TMP_InputField quickAttackChanceInput;
+        [SerializeField] private TMP_InputField strongAttackDamageInput;
+        [SerializeField] private TMP_InputField strongAttackChanceInput;
+
+        [SerializeField] private Button closeButton;
+
+        private bool _isOpened;
+        private bool _isSliding;
+
+        private const float SlidingSpeed = .6f;
+        private const float SlidingOffset = 850f;
+
+        private void Start()
+        {
+            closeButton.onClick.AddListener(OnClose);
+            spawner.OnClickEventHandler += SpawnerOnClickEventHandler;
+        }
+
+        private void SpawnerOnClickEventHandler(object sender, EventArgs e)
+        {
+            SlideOpen();
+        }
+
+        private void Awake()
+        {
+            FillSettingsForm(spawner.GetCurrentSettings());
+        }
+
+        private void SlideOpen()
+        {
+            if (_isSliding || _isOpened)
+            {
+                return;
+            }
+        
+            _isOpened = true;
+            StartCoroutine(Sliding());
+        }
+    
+        private void SlideClose()
+        {
+            if (_isSliding || !_isOpened)
+            {
+                return;
+            }
+        
+            _isOpened = false;
+            StartCoroutine(Sliding());
+            StartCoroutine(Sliding());
+        }
+ 
+        private IEnumerator Sliding()
+        {
+            _isSliding = true;
+            var startingPosition  = transform.position;
+            var finalPosition = new Vector3(transform.position.x + SlidingOffset * (_isOpened ? -1 : 1),
+                transform.position.y,
+                transform.position.z);
+
+            float elapsedTime = 0;
+            while (elapsedTime < SlidingSpeed)
+            {
+                transform.position = Vector3.Lerp(startingPosition, finalPosition, (elapsedTime / SlidingSpeed));
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            _isSliding = false;
+        }
+
+        private void OnClose()
+        {
+            var unitSettings = GetSettingsFromForm();
+            spawner.ChangeUnitSettings(unitSettings);
+
+            SlideClose();
+        }
+
+        private UnitSettings GetSettingsFromForm()
+        {
+            var unitSettings = spawner.GetCurrentSettings();
+
+            maxHealthInput.TryGetValueTo(ref unitSettings.maxHealth);
+            speedInput.TryGetValueTo(ref unitSettings.speed);
+            critChanceInput.TryGetValueTo(ref unitSettings.critChance);
+            missChanceInput.TryGetValueTo(ref unitSettings.missChance);
+            quickAttackDamageInput.TryGetValueTo(ref unitSettings.quickAttack.damage);
+            quickAttackChanceInput.TryGetValueTo(ref unitSettings.quickAttack.chanceToUse);
+            strongAttackDamageInput.TryGetValueTo(ref unitSettings.strongAttack.damage);
+            strongAttackChanceInput.TryGetValueTo(ref unitSettings.strongAttack.chanceToUse);
+
+            return unitSettings;
+        }
+
+        private void FillSettingsForm(UnitSettings settings)
+        {
+            maxHealthInput.text = $"{settings.maxHealth}";
+            speedInput.text = $"{settings.speed}";
+            critChanceInput.text = $"{settings.critChance}";
+            missChanceInput.text = $"{settings.missChance}";
+            quickAttackDamageInput.text = $"{settings.quickAttack.damage}";
+            quickAttackChanceInput.text = $"{settings.quickAttack.chanceToUse}";
+            strongAttackDamageInput.text = $"{settings.strongAttack.damage}";
+            strongAttackChanceInput.text = $"{settings.strongAttack.chanceToUse}";
+        }
+
+        private void OnDisable()
+        {
+            spawner.OnClickEventHandler -= SpawnerOnClickEventHandler;
+        }
+    }
+}
